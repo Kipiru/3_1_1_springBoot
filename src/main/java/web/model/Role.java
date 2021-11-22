10ad07a01b6e26e398bb066ac09c54fd6d1dd85f
Kipiru <dtaranets@gmail.com>
2021-11-22 21:44:11 +0300

    remove SpringSecurityInitializer; edit page get roles from database now; remove JsonIgnore in Role;

--- src/main/java/web/model/Role.java
+++ src/main/java/web/model/Role.java
@@ -1,6 +1,6 @@
 package web.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
@@ -17,7 +17,7 @@ public class Role implements GrantedAuthority {
 
     private String role;
 
-    @JsonIgnore
+
     @ManyToMany(mappedBy = "roles")
 
     private Set<User> users = new HashSet<>();
